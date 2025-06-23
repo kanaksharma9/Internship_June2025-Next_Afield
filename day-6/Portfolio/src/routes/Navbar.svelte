db8e0be1225061dfd3be4d1d448feb283b8149df
Kanak sharma <sharmakanak043@gmail.com>
2025-06-23 15:04:48 +0530

    day-6 done

---
+++ day-6/Portfolio/src/routes/Navbar.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<nav>
+    <div class='wrapper'>
+    <div class='logo'>
+        Kanak Sharma
+    </div>
+    <div class='elements'>
+        About
+        Experience
+        Projects
+    </div>
+    </div>
+</nav>
+
+<style>
+    nav {
+        height: fit-content;
+        width: 1000px;
+    }
+    .wrapper{
+        flex-direction: ;
+    }
+    .logo{
+        align-items: first baseline;
+    }
+    .elements{
+        align-items: end;
+    }
+</style>
