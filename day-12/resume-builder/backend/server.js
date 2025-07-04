fbc7266184c795fa881eb273cd349ae70b2a35d5
Kanak sharma <sharmakanak043@gmail.com>
2025-07-04 15:03:07 +0530

    day-15 done

---
+++ day-12/resume-builder/backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/resume", require("./routes/resumeRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
